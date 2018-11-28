15e2db1b96886938705116ad26bade0606383657
Christopher Brembach <Christopher.Brembach@gmail.com>
2018-11-28 14:53:54 +0100

    Initial Commit

---
+++ 2d detectionTests/_d_detectionTests.swift
@@ -0,0 +1,34 @@
+//
+//  _d_detectionTests.swift
+//  2d detectionTests
+//
+//  Created by Christopher brembach on 28.11.18.
+//  Copyright © 2018 Christopher brembach. All rights reserved.
+//
+
+import XCTest
+@testable import _d_detection
+
+class _d_detectionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
