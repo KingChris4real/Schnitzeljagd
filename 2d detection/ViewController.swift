e1c0c066b6878b59b4d290c71817756d40fd5276
Christopher Brembach <Christopher.Brembach@gmail.com>
2019-06-28 12:19:56 +0200

    init commit

--- 2d detection/ViewController.swift
+++ 2d detection/ViewController.swift
@@ -1,9 +1,9 @@
 //
 //  ViewController.swift
-//  2d detection
+//  Abgabe CC6 2019
 //
-//  Created by Christopher brembach on 28.11.18.
-//  Copyright © 2018 Christopher brembach. All rights reserved.
+//  Created by Christopher Brembach on 05.06.19
+//  Copyright © 2019 Christopher Brembach. All rights reserved.
 //
 
 import UIKit
@@ -14,17 +14,53 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    @IBOutlet weak var K: UILabel!
+    
+    @IBOutlet weak var R: UILabel!
+    
+    @IBOutlet weak var O: UILabel!
+    
+    @IBOutlet weak var N: UILabel!
+    
+    @IBOutlet weak var E: UILabel!
+    
+    @IBOutlet weak var holIhnDir: UIButton!
+    
+    @IBAction func zurück(_ sender: Any) {
+    }
+    
+    @IBAction func mapslink(_ sender: Any) {
+    }
+    /*@IBAction func show(sender: AnyObject) {
+        /*if K.isHidden // condition to check if the button is hidden -
+            {
+                K.isHidden = true // set the button to be visible
+        }*/
+       
+    }*/
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        K.isHidden = true
+        R.isHidden = true
+        O.isHidden = true
+        N.isHidden = true
+        E.isHidden = true
+        holIhnDir.isHidden = true
         
+        func zurück(_ sender: Any) {
+            holIhnDir.isHidden = false
+        }
+    
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        //sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/game.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -34,7 +70,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
+        //let configuration = ARWorldTrackingConfiguration()
+
+        guard let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "Photos", bundle: nil) else {
+            print("No images available")
+            return
+        }
+        
+        ////
+        configuration.trackingImages = trackedImages
+        //configuration.detectionImages = trackedImages
+        //configuration.maximumNumberOfTrackedImages = 2
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -47,29 +94,134 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
+//
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+         
         let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
         
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 0, alpha: 0.0)
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi / 2
         
-    }
+            if case imageAnchor.referenceImage.name = "1"{
+                K.isHidden = false
+            }
+            if case imageAnchor.referenceImage.name = "2"{
+                R.isHidden = false
+            }
+            if case imageAnchor.referenceImage.name = "3"{
+                O.isHidden = false
+            }
+            if case imageAnchor.referenceImage.name = "4"{
+                N.isHidden = false
+            }
+            if case imageAnchor.referenceImage.name = "5"{
+                E.isHidden = false
+            }
+            if E.isHidden == false && K.isHidden == false && R.isHidden == false && O.isHidden == false && N.isHidden == false {
+                holIhnDir.isHidden = false
+            }
+            
+        switch imageAnchor.referenceImage.name {
+        case "1" :
+                let ship1Scene = SCNScene(named: "art.scnassets/102.scn")!
+                let ship1Node = ship1Scene.rootNode.childNodes.first!
+                ship1Node.position = SCNVector3Zero
+                ship1Node.position.z = 0.01
+                //ship1Node.position.x = 0.15
+                
+                planeNode.addChildNode(ship1Node)
+                node.addChildNode(planeNode)
+            
+            
+             case "2" :
+             let ship2Scene = SCNScene(named: "art.scnassets/1003.scn")!
+             let ship2Node = ship2Scene.rootNode.childNodes.first!
+             ship2Node.position = SCNVector3Zero
+             ship2Node.position.z = 0.00
+             //ship2Node.position.x = 0.15
+             
+             planeNode.addChildNode(ship2Node)
+             node.addChildNode(planeNode)
+             
+            case "3" :
+                let ship3Scene = SCNScene(named: "art.scnassets/1004.scn")!
+                let ship3Node = ship3Scene.rootNode.childNodes.first!
+                ship3Node.position = SCNVector3Zero
+                ship3Node.position.z = 0.01
+                //ship3Node.position.x = 0.15
+                
+                planeNode.addChildNode(ship3Node)
+                node.addChildNode(planeNode)
+                
+                
+                
+            case "4" :
+                let ship4Scene = SCNScene(named: "art.scnassets/1005.scn")!
+                let ship4Node = ship4Scene.rootNode.childNodes.first!
+                ship4Node.position = SCNVector3Zero
+                ship4Node.position.z = 0.01
+                //ship4Node.position.x = 0.15
+                
+                planeNode.addChildNode(ship4Node)
+                node.addChildNode(planeNode)
+                
+            case "5" :
+                let ship5Scene = SCNScene(named: "art.scnassets/1001.scn")!
+                let ship5Node = ship5Scene.rootNode.childNodes.first!
+                ship5Node.position = SCNVector3Zero
+                ship5Node.position.z = 0.01
+                //ship5Node.position.x = 0.15
+                
+                planeNode.addChildNode(ship5Node)
+                node.addChildNode(planeNode)
+                
+                
+            case .none:
+                print("nothing found")
+            case .some(_):
+                print("multiple found")
+               
+            }
+            
+            /*let shipScene = SCNScene(named: "art.scnassets/ship.scn")!
+            let shipNode = shipScene.rootNode.childNodes.first!
+            shipNode.position = SCNVector3Zero
+            shipNode.position.z = 0.15
+            
+            planeNode.addChildNode(shipNode)
+            
+            
+            node.addChildNode(planeNode)*/
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+                
+            
+            /*if imageAnchor.referenceImage.name == "IMG_1866" {
+                // Set AVPlayer as the plane's texture and play
+             // nach oben ziehen und nur plane node placen?
+             let shipScene = SCNScene(named: "art.scnassets/ship.scn")!
+                let shipNode = shipScene.rootNode.childNodes.first!
+                shipNode.position = SCNVector3Zero
+                shipNode.position.z = 0.15
+                
+                planeNode.addChildNode(shipNode)
+                
+            } else if imageAnchor.referenceImage.name == "IMG_1867" {
+                let ship2Scene = SCNScene(named: "art.scnassets/ship.scn")!
+                let ship2Node = ship2Scene.rootNode.childNodes.first!
+                ship2Node.position = SCNVector3Zero
+                ship2Node.position.z = 0.15
+                
+                planeNode.addChildNode(ship2Node)
+            }*/
+ 
+            
+        }
+    return node
     }
 }
